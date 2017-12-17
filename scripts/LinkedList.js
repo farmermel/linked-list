0380cc620aac3cb4e10bc459db89fe5e86a02dc9
Melena Suliteanu <melenasuliteanu@gmail.com>
2017-12-17 10:18:15 -0700

    Finish functionality for pop method

--- scripts/LinkedList.js
+++ scripts/LinkedList.js
@@ -11,6 +11,7 @@ export default class LinkedList {
       this.head = new Node(data);
     } else {
       let oldHead = this.head;
+
       this.head = new Node(data); 
       this.head.next = oldHead;
     }
@@ -22,11 +23,48 @@ export default class LinkedList {
       return null;
     }
     let currentHead = this.head;
+
     this.head = this.head.next;
     return currentHead;
   }
 
-  push() {
+  push(data) {
+    if(!this.head) {
+      this.head = new Node(data);
+    } else {
+      let current = this.head;
+
+      while(current.next != null) {
+        current = current.next;
+      }
+      current.next = new Node(data);
+    }
+    this.length++;
+  }
+
+  pop() {
+    if(!this.length) {
+      this.length = this.length
+      return null;
+    } 
+      let current = this.head;
+      let previous = this.head;
+
+      if(!this.head.next) {
+        this.head = null;
+      }
+
+      while(current.next != null) {
+        previous = current;
+        current = current.next;
+
+      }
+      let toReturn = current;
+
+      previous.next = null;
+      this.length--;
+      return toReturn;
+    
     
   }
 }
